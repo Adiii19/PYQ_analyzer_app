72fbb4a6e25167126c3e9b70e660625535d15421
Adiii19 <2022.aditya.mhatre@ves.ac.in>
2025-10-19 16:26:51 +0530

    retrurn of app.py

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+import re
+from clustering_module import (
+    extract_questions_from_pdf,
+    preprocess_questions,
+    group_similar_questions,
+    label_clusters_by_frequency,
+    group_clusters_by_label,
+    generate_pca_plot
+)
+
+
+
